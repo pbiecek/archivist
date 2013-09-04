970c302b1f4e1c68abff2eccf1d3ee881fbb8672
pbiecek <przemyslaw.biecek@gmail.com>
2013-09-04 12:23:31 +0200

    updated init.R, more interesting example

--- init.R
+++ init.R
@@ -4,11 +4,18 @@ install_github("archivist", "pbiecek")
 library(archivist)
 source("../tokens.R")
 
-pl <- qplot(1, 1, data=iris)
+plotOb <- getBinaryURL("https://raw.github.com/pbiecek/graphGallery/master/77514920247a0a2eca602286cf8a4c60/plot.rda")
+tf <- tempfile()
+writeBin(plotOb, tf)
+name <- load(tf)
+unlink(tf)
+get(name)
+name
 
-refs <- dd(pl, archiveData=TRUE,
-         archiveWrite=archivePlotWrite, archiveRead=archivePlotRead, 
-         archiveWriteData=archiveDataWrite, archiveReadData=archiveDataRead)
+
+refs <- dd(plotOb, archiveData=TRUE,
+           archiveWrite=archivePlotWrite, archiveRead=archivePlotRead, 
+           archiveWriteData=archiveDataWrite, archiveReadData=archiveDataRead)
 
 (plref <- getBitLy(refs$plot.ref, mytoken)$bitly)
 (dtref <- getBitLy(refs$data.ref, mytoken)$bitly)
@@ -16,6 +23,7 @@ refs <- dd(pl, archiveData=TRUE,
 plotQRcode(plref)
 plotQRcode(dtref)
 
-pl
+plotOb
 addQR2ggplot(plref)
+
 
