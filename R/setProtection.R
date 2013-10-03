41e162ea6824b094d70eefc8f2d7cf5684628dd8
pbiecek <przemyslaw.biecek@gmail.com>
2013-10-03 11:34:24 +0200

    now links to data is stores in links_data.md5

--- R/setProtection.R
+++ R/setProtection.R
@@ -1,4 +1,4 @@
-setProtection <- function(md5links, archiveWriteDir, period = days(1), fixedDate = now() + period) {
+setProtection <- function(md5links, archiveWriteDir, period = months(1), fixedDate = now() + period) {
   require(lubridate)
   sapply(md5links, function(md5l) {
     cat(as.character(fixedDate), file=paste0(archiveWriteDir, md5l, "/protectionDate.txt"))
