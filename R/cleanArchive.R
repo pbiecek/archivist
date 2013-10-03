4531266d5b346a992e663a817835a4a6f2416509
pbiecek <przemyslaw.biecek@gmail.com>
2013-10-03 11:09:27 +0200

    function that clean the archive

---
+++ R/cleanArchive.R
@@ -0,0 +1,21 @@
+# remove directories without protectionDate.txt
+# or with older date than dueDate
+cleanArchive <- function(archiveWriteDir, dueDate = now()) {
+  require(lubridate)
+  md5links <- list.files(archiveWriteDir)
+  sapply(md5links, function(md5l) {
+    if (file.exists(paste0(archiveWriteDir, md5l, "/protectionDate.txt"))) {
+      content <- readLines(paste0(archiveWriteDir, md5l, "/protectionDate.txt"))
+      res <- try(as.POSIXct(content) - dueDate > 0, silent=TRUE)
+      if (class(res) != "try-error" && length(res)>0 && res) {
+        return(paste0("protected till ",content))
+      } else { # remove, protection expired
+        unlink(paste0(archiveWriteDir, md5l), recursive = TRUE)
+        return("removed, protection expired")
+      }
+    } else { # remove, no protection
+      unlink(paste0(archiveWriteDir, md5l), recursive = TRUE)
+      return("removed, no protection")
+    }
+  })
+}
