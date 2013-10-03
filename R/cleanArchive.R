bfdfb2273ffeec09ee8b8e32ea01c9cb2ea87448
pbiecek <przemyslaw.biecek@gmail.com>
2013-10-03 11:17:32 +0200

    fixed date checking

--- R/cleanArchive.R
+++ R/cleanArchive.R
@@ -5,7 +5,7 @@ cleanArchive <- function(archiveWriteDir, dueDate = now()) {
   md5links <- list.files(archiveWriteDir)
   sapply(md5links, function(md5l) {
     if (file.exists(paste0(archiveWriteDir, md5l, "/protectionDate.txt"))) {
-      content <- readLines(paste0(archiveWriteDir, md5l, "/protectionDate.txt"))
+      suppressWarnings(content <- readLines(paste0(archiveWriteDir, md5l, "/protectionDate.txt")))
       res <- try(as.POSIXct(content) - dueDate > 0, silent=TRUE)
       if (class(res) != "try-error" && length(res)>0 && res) {
         return(paste0("protected till ",content))
--- R/setProtection.R
+++ R/setProtection.R
@@ -1,14 +1,8 @@
-setProtection <- function(md5links, archiveWriteDir, period = days(1), fixed = NULL) {
+setProtection <- function(md5links, archiveWriteDir, period = days(1), fixedDate = now() + period) {
   require(lubridate)
-  till <- now()
-  if (is.null(fixed)) {
-    till <- fixed
-  } else {
-    till <- now() + period
-  }
   sapply(md5links, function(md5l) {
-    cat(as.character(till), file=paste0(archiveWriteDir, md5l, "/protectionDate.txt"))
-    as.character(till)
+    cat(as.character(fixedDate), file=paste0(archiveWriteDir, md5l, "/protectionDate.txt"))
+    as.character(fixedDate)
   })
 }
 
