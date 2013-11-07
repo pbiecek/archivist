5af37b0a1d88e34c9255cb75d3deb1b47bd707a3
pbiecek <przemyslaw.biecek@gmail.com>
2013-11-07 17:13:36 +0100

    extract tags for data frame and lm

--- R/extractTags.R
+++ R/extractTags.R
@@ -2,7 +2,19 @@ extractTags <- function (object, ... )
   UseMethod("extractTags")
 
 extractTags.default <- function(object, ...) {
+  
 }
 
+extractTags.lm <- function(object, ...) {
+  unique(c(names(object$coefficients), 
+           as.character(object$terms),
+           colnames(object$model),
+           "linear regression model"))
+}
+
+extractTags.data.frame <- function(object, ...) {
+  unique(c(colnames(object),
+           "data frame"))
+}
 
 
