3b3037c3d3855bec4ab66efc1aa992b56f715450
pbiecek <przemyslaw.biecek@gmail.com>
2013-11-07 12:32:14 +0100

    examples for database manilpulation

--- R/databaseManager.R
+++ R/databaseManager.R
@@ -4,16 +4,39 @@ setUpDatabase <- function() {
   sqlite    <- dbDriver("SQLite")
   backpack <- dbConnect(sqlite,"database/backpack.db")
   
-  artifact <- data.frame(md5hash = "", class = "", createdDate = now(), stringsAsFactors=FALSE)
-  setting <- data.frame(name = c("localPath", "remotePath"), value=c("...", "..."), createdDate = now(), stringsAsFactors=FALSE)
+  artifact <- data.frame(md5hash = "", 
+                         class = "", 
+                         createdDate = now(), 
+                         pathToWelcomePage = ""
+                         stringsAsFactors=FALSE)
+  
   relation <- data.frame(artifactFrom = "", artifactTo = "", relationName = now(), stringsAsFactors=FALSE)
   tag <- data.frame(artifact = "", tag = "", relationName = now(), stringsAsFactors=FALSE)
+
+  setting <- data.frame(name = c("localPathToArchive",
+                                 "externalPathToArchive",
+                                 "miniatureFormat",
+                                 "miniatureWidth",
+                                 "miniatureHeight"), 
+                        value=c("/Users/pbiecek/camtasia/GitHub/graphGallery/", 
+                                "https://github.com/pbiecek/graphGallery/master/",
+                                "png",
+                                "800",
+                                "600"), 
+                        createdDate = now(), stringsAsFactors=FALSE)
+  
+  
+  
   
   dbListTables(backpack)
-  dbWriteTable(backpack,"artifact",artifact)
-  dbWriteTable(backpack,"setting",setting)
-  dbWriteTable(backpack,"relation",relation)
-  dbWriteTable(backpack,"tag",tag)
+  dbWriteTable(backpack, "artifact",artifact, overwrite=TRUE)
+  dbWriteTable(backpack, "setting",setting, overwrite=TRUE)
+  dbWriteTable(backpack, "relation",relation, overwrite=TRUE)
+  dbWriteTable(backpack, "tag",tag, overwrite=TRUE)
+  
+  
+  dbGetQuery(backpack, "select * from setting")
+  
   
 }
 
