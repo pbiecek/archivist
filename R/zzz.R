41e72b599a6491795934e2c361001b9014b5b9d6
pbiecek <przemyslaw.biecek@gmail.com>
2013-11-08 18:10:51 +0100

    fixed database in package environment

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,18 @@
-library("RSQLite")
-sqlite    <- dbDriver("SQLite")
-
 .ArchivistEnv <- new.env()
-assign(".backpack", 
-       dbConnect(sqlite,paste0(path.package("archivist"), "/database/backpack.db")), 
-       envir = .ArchivistEnv)
+
+.onLoad <- function(libname, pkgname) {
+  library("RSQLite")
+  sqlite    <- dbDriver("SQLite")
+  assign(".backpack", 
+         dbConnect(sqlite,paste0(libname, "/database/backpack.db")), 
+         envir = .ArchivistEnv)
+  
+}
+.onAttach <- function(libname, pkgname) {
+  .onLoad(libname, pkgname)  
+}
+
+.onDetach <- function(libpath) {
+  dbDisconnect(get(".backpack", envir = .ArchivistEnv))
+}
+
