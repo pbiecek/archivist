e61e7ae2818fe31cbf8b9e84aa9cef4740c6dc63
pbiecek <przemyslaw.biecek@gmail.com>
2013-11-08 18:11:30 +0100

    browser trap

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@
 .onLoad <- function(libname, pkgname) {
   library("RSQLite")
   sqlite    <- dbDriver("SQLite")
+  browser()
   assign(".backpack", 
          dbConnect(sqlite,paste0(libname, "/database/backpack.db")), 
          envir = .ArchivistEnv)
