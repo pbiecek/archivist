343ed0afcd0159f87eb4609a9e910797edb66a20
pbiecek <przemyslaw.biecek@gmail.com>
2013-11-08 18:13:54 +0100

    moved from .onLoad to .onAttached

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,14 @@
 .ArchivistEnv <- new.env()
 
 .onLoad <- function(libname, pkgname) {
+}
+
+.onAttach <- function(libname, pkgname) {
   library("RSQLite")
   sqlite    <- dbDriver("SQLite")
   assign(".backpack", 
          dbConnect(sqlite,paste0(path.package("archivist"), "/database/backpack.db")), 
          envir = .ArchivistEnv)
-  
-}
-.onAttach <- function(libname, pkgname) {
-  .onLoad(libname, pkgname)  
 }
 
 .onDetach <- function(libpath) {
