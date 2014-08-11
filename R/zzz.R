b14673162251a99a058e77387a41e4efcf723147
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2014-08-11 16:00:44 +0200

    docco style vignette

--- R/zzz.R
+++ R/zzz.R
@@ -3,12 +3,14 @@
 }
 .ArchivistEnv <- new.env()
 
-.onDetach <- function(libpath) {
-  if (".backpack" %in% ls(.ArchivistEnv)) {
-    tmp <- get(".backpack", envir = .ArchivistEnv)
-    if (!is.null(tmp)) {
-      dbDisconnect(tmp)
-      assign(".backpack", NULL, envir = .ArchivistEnv)
-    }
-  } 
-}
+# .onDetach <- function(libpath) {
+#   if (".backpack" %in% ls(.ArchivistEnv)) {
+#     tmp <- get(".backpack", envir = .ArchivistEnv)
+#     if (!is.null(tmp)) {
+#       dbDisconnect(tmp)
+#       assign(".backpack", NULL, envir = .ArchivistEnv)
+#     }
+#   } 
+# }
+
+
