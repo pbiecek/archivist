d084aa54bb070f836c60714c97a496e78b868bec
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2014-09-08 13:24:15 +0200

    classic docco style template vignette test

--- buildingVignette.R
+++ buildingVignette.R
@@ -3,7 +3,13 @@
 # sudo npm install -g docco
 
 
-knitr::knit2html(input = "vignettes/archivist.Rmd", 
-                 output = "vignettes/archivist.md", 
+knitr::knit2html(input = "vignettes/chainingUseCase.Rmd", 
+                 output = "vignettes/chainingUseCase.md", 
                  template = system.file("misc", "docco-template.html", 
                  package = "knitr"))
+
+
+knitr::knit2html(input = "vignettes/lazyLoadUseCase.Rmd", 
+                 output = "vignettes/lazyLoadUseCase.md", 
+                 template = system.file("misc", "docco-template.html", 
+                                        package = "knitr"))
