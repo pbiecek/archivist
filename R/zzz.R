eacb781e891937933d88f54f57985c3050637a7d
pbiecek <przemyslaw.biecek@gmail.com>
2014-09-11 14:49:35 +0200

    version 1.0 -> 1.1, preparation for CRAN release

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .ArchivistEnv <- new.env()
 
 .onAttach <- function(...) {
-  packageStartupMessage( "\n Welcome to the archivist package (ver 1.0)." )
+  packageStartupMessage( "\n Welcome to the archivist package (ver 1.1)." )
   assign( x = "sqlite", value = dbDriver( "SQLite" ), envir = .ArchivistEnv )
   assign( x = ".GithubURL", value = "https://raw.githubusercontent.com/", envir = .ArchivistEnv )
 }
