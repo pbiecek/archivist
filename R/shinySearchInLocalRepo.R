0b14aba923bba5e24bc482ebda20d266365f97c7
pbiecek <przemyslaw.biecek@gmail.com>
2014-11-22 22:15:45 +0100

    sorting in shiny search is added, as requested in #51

--- R/shinySearchInLocalRepo.R
+++ R/shinySearchInLocalRepo.R
@@ -17,6 +17,11 @@
 #' text input in a shiny application. Many tags may be specified, they should be comma separated. 
 #' User can specify more tags when artifact is created, tags like phase, project, author etc.
 #' 
+#' In the search query one can add tags started with sort: or sort:-
+#' then miniatures will be sorted accordingly.
+#' For exaple sort:class will sort along class tag, while sort:-class backward.
+#' sort:createdDate will sort along createdDate and sort:-createdDate backward.
+#' 
 #' \code{Tags}, submitted in the text field, should be given according to the 
 #' format: \code{"TagType:TagTypeValue"} - see examples.
 #'   
@@ -61,7 +66,35 @@ shinySearchInLocalRepo <- function( repoDir, host = '0.0.0.0' ){
     server = function(input, output) {
       output$plot <- renderUI({ 
         tags <- strsplit(input$search, split=" *, *")[[1]]
-        md5s <- multiSearchInLocalRepo( tags, repoDir, fixed = FALSE, intersect = TRUE )
+        sortTags <- grep(tags, pattern="^sort:")
+        # tags with sort: key should be removed from search
+        if (length(sortTags) > 0) {
+          md5s <- multiSearchInLocalRepo( tags[-sortTags], repoDir, fixed = FALSE, intersect = TRUE )
+        } else {
+          md5s <- multiSearchInLocalRepo( tags, repoDir, fixed = FALSE, intersect = TRUE )
+        }
+        # should we sort results
+        if (length(sortTags) > 0 & length(md5s) > 0) {
+          allTags <- showLocalRepo(repoDir, method = "tags")
+          selectedTags <- allTags[allTags$artifact %in% md5s,]
+          if (tags[sortTags[1]] == "sort:createdDate") {
+            md5s <- unique(selectedTags$artifact[order(selectedTags$createdDate)])
+          } else {
+            if (tags[sortTags[1]] == "sort:-createdDate") {
+              md5s <- unique(selectedTags$artifact[order(selectedTags$createdDate, decreasing = TRUE)])
+            } else {
+              # if there is - before sort condition
+              key <- substr(tags[sortTags[1]], 6, 1000)
+              if (substr(key, 1, 1) == "-") {
+                tmpTags <- selectedTags[grep(selectedTags$tag,pattern = substr(key, 2, 1000)),,drop=FALSE]
+                md5s <- unique(tmpTags[order(tmpTags$tag, na.last = TRUE),"artifact"])
+              } else {
+                tmpTags <- selectedTags[grep(selectedTags$tag,pattern = key),,drop=FALSE]
+                md5s <- unique(tmpTags[order(tmpTags$tag),"artifact"])
+              }
+            }
+          }
+        }
         
         addResourcePath(
           prefix="repo", 
