e36bbda954244f14e7eee3c8f1adcfb3f85ca7bd
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-09-02 19:32:22 +0200

    all tests after added ggplot2 to DESC in suggests field and after loading it in testthar.R

--- tests/testthat/test_base_functionalities.R
+++ tests/testthat/test_base_functionalities.R
@@ -1,20 +1,20 @@
-# test_that("setLocalRepo sets repo", {
-#   setLocalRepo(repoDir = "tmp_archivist")
-#   expect_equal(get(".repoDir", envir = .ArchivistEnv), "tmp_archivist/")
-#   
-#   setGithubRepo("MarcinKosinski", "archivist", "master", "clone123")
-#   expect_equal(get(".user", envir = .ArchivistEnv), "MarcinKosinski")
-#   expect_equal(get(".repo", envir = .ArchivistEnv), "archivist")
-#   expect_equal(get(".branch", envir = .ArchivistEnv), "master")
-#   expect_equal(get(".repoDirGit", envir = .ArchivistEnv), "clone123")
-# })
-# 
-# test_that(".ArchivistEnv exists", {
-#   expect_equal(get("sqlite", envir = .ArchivistEnv), DBI::dbDriver( "SQLite" ))
-#   expect_equal(get(".GithubURL", envir = .ArchivistEnv), "https://raw.githubusercontent.com/")
-# })
-# 
-# test_that("aoptions sets options", {
-#   aoptions(".repoDir", "test123")
-#   expect_equal(get(".repoDir", envir = .ArchivistEnv), "test123")
-# })
+test_that("setLocalRepo sets repo", {
+  setLocalRepo(repoDir = "tmp_archivist")
+  expect_equal(get(".repoDir", envir = .ArchivistEnv), "tmp_archivist/")
+  
+  setGithubRepo("MarcinKosinski", "archivist", "master", "clone123")
+  expect_equal(get(".user", envir = .ArchivistEnv), "MarcinKosinski")
+  expect_equal(get(".repo", envir = .ArchivistEnv), "archivist")
+  expect_equal(get(".branch", envir = .ArchivistEnv), "master")
+  expect_equal(get(".repoDirGit", envir = .ArchivistEnv), "clone123")
+})
+
+test_that(".ArchivistEnv exists", {
+  expect_equal(get("sqlite", envir = .ArchivistEnv), DBI::dbDriver( "SQLite" ))
+  expect_equal(get(".GithubURL", envir = .ArchivistEnv), "https://raw.githubusercontent.com/")
+})
+
+test_that("aoptions sets options", {
+  aoptions(".repoDir", "test123")
+  expect_equal(get(".repoDir", envir = .ArchivistEnv), "test123")
+})
