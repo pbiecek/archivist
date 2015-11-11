5db32d0004f3549e22b9dbe46cba46c94ee00d8d
witia1990 <wc291520@students.mimuw.edu.pl>
2015-11-11 15:03:03 +0100

    Alterations of the text of `getTags` file. Additional example using `getTagsLocal`

--- R/getTags.R
+++ R/getTags.R
@@ -1,35 +1,45 @@
 ##    archivist package for R
 ##
-#' @title Return a Tag Corresponding to md5hash
+#' @title Return Tags Corresponding to md5hash
 #'
 #' @description
-#' \code{getTagsLocal} and \code{getTagsGithub} return a \code{Tag} (see \link{Tags}) related to \link{md5hash} 
-#' of an artifact. To learn more about artifacts visit \link[archivist]{archivist-package}.
+#' \code{getTagsLocal} and \code{getTagsGithub} return \code{Tags} (see \link{Tags})
+#' related to \link{md5hash} of an artifact. To learn more about artifacts visit
+#' \link[archivist]{archivist-package}.
 #' 
 #' @details
-#' \code{getTagsLocal} and \code{getTagsGithub} return (see \link{Tags}) related to \link{md5hash} 
-#' of an artifact. To learn more about artifacts visit \link[archivist]{archivist-package}.
+#' \code{getTagsLocal} and \code{getTagsGithub} return \code{Tags}, of a specific type described
+#' by \code{tag} parameter, related to \link{md5hash} of an artifact. To learn more about 
+#' artifacts visit \link[archivist]{archivist-package}.
 #'  
-#' @return The character is returned, which is a \code{Tag} (see \link{Tags}) related to \link{md5hash} 
+#' @return The character vector of \code{Tags} (see \link{Tags}) related to \link{md5hash} 
 #' of an artifact.
 #'
 #' @param repoDir A character denoting an existing directory in 
-#' which an artifact is stored. If set to \code{NULL} (by default), uses the \code{repoDir} specified in \link{setLocalRepo}.
+#' which artifacts are stored. If set to \code{NULL} (by default),
+#' uses the \code{repoDir} specified in \link{setLocalRepo}.
 #' 
 #' @param md5hash A character containing \code{md5hash} 
-#' of artifacts which \code{Tag} is desired to be returned.
+#' of artifacts which \code{Tags} are desired to be returned.
 #' 
-#' @param tag A type of a \link{Tags}. Default \code{tag = "name"}.
+#' @param tag A regular expression denoting type of a \code{Tag} that we search for
+#' (see \code{Examples}). Default \code{tag = "name"}.
 #' 
-#' @param repo Only if working with a Github repository. A character containing a name of a Github repository on which the Repository is archived.
+#' @param repo While working with the Github repository. A character containing
+#' a name of the Github repository on which the Repository is stored.
 #' By default set to \code{NULL} - see \code{Note}.
-#' @param user Only if working with a Github repository. A character containing a name of a Github user on whose account the \code{repo} is created.
+#' 
+#' @param user While working with the Github repository. A character containing
+#' a name of the Github user on whose account the \code{repo} is created.
 #' By default set to \code{NULL} - see \code{Note}.
-#' @param branch Only if working with a Github repository. A character containing a name of 
-#' Github repository's branch in which Repository is archived. Default \code{branch} is \code{master}.
+#' 
+#' @param branch While working with the Github repository. A character containing
+#' a name of the Github repository's branch on which the Repository is stored.
+#' Default \code{branch} is \code{master}.
 #'
-#' @param repoDirGit Only if working with a Github repository. A character containing a name of a directory on Github repository 
-#' on which the Repository is stored. If the Repository is stored in main folder on Github repository, this should be set 
+#' @param repoDirGit While working with the Github repository. A character containing
+#' a name of a directory on the Github repository on which the Repository is stored.
+#' If the Repository is stored in main folder on the Github repository, this should be set 
 #' to \code{repoDirGit = FALSE} as default.
 #' 
 #' @note
@@ -41,31 +51,75 @@
 #'
 #' @examples
 #' \dontrun{
-#' library(dplyr)
+#' 
+#' ### Local version
+#' 
+#' ## EXAMPLE with pipe operator %a%
+#' 
+#' # Creating empty repository
 #' exampleRepoDir <- tempfile()
 #' createEmptyRepo( exampleRepoDir )
 #' 
+#' library(dplyr)
 #' data(mtcars)
-#' hash <- mtcars %.% 
-#'  group_by(cyl, am) %.%
-#'  select(mpg, cyl, wt, am) %.%
-#'  summarise(avgmpg = mean(mpg), avgwt = mean(wt)) %.%
-#'  filter(avgmpg > 20) %>%
-#'  saveToRepo( exampleRepoDir )
-#'  
+#' setLocalRepo(repoDir = exampleRepoDir)
+#' hash <- mtcars %a% 
+#'  group_by(cyl, am) %a%
+#'    select(mpg, cyl, wt, am) %a%
+#'      summarise(avgmpg = mean(mpg), avgwt = mean(wt)) %a%
+#'        filter(avgmpg > 20) %a%
+#'        saveToRepo( exampleRepoDir )
+#'        
+#' showLocalRepo(exampleRepoDir)
+#' showLocalRepo(exampleRepoDir, method = "tags")
+#' 
+#' # We search for a tag with default "name" regular expression corresponding to 
+#' # hash md5hash.
 #' getTagsLocal( md5hash = hash, exampleRepoDir )
+#' 
+#' # Deleting example respository
+#' deleteRepo( exampleRepoDir, TRUE) 
+#' rm( exampleRepoDir ) 
+#' 
+#' ## EXAMPLE with data iris
+#' exampleRepoDir <- tempfile()
+#' createEmptyRepo( exampleRepoDir )
+#' 
+#' data(iris)
+#' saveToRepo(iris, repoDir = exampleRepoDir )
+#' showLocalRepo(exampleRepoDir)
+#' showLocalRepo(exampleRepoDir, method = "tags")
+#' 
+#' # We can notice that there is only one md5hash in repo so we will use it
+#' hash <- showLocalRepo(exampleRepoDir)[,1]
+#' 
+#' # We search for a tag with "varname" regular expression corresponding to 
+#' # hash md5hash.
+#' getTagsLocal( md5hash = hash, exampleRepoDir, tag = "varname" ) 
+#' # There are 5 different tags with "varname" regular expression
+#'
+#' # We needn't use the whole expression "varname". We may use its abbreviation
+#' # and get the same result.
+#' getTagsLocal( md5hash = hash, exampleRepoDir, tag = "varna" ) 
 #' 
 #' deleteRepo( exampleRepoDir, TRUE) 
 #' rm( exampleRepoDir ) 
 #' 
-#' getTagsGithub( "3db63bc63b8defaf42c0bde19160f242", 
-#'    user="pbiecek", repo="archivist")
+#' ### Github version
+#' ## EXAMPLE: pbiecek archivist repository on Github
 #' 
-#' # many archivist-like Repositories on one Github repository
-#' 
-#' getTagsGithub("ff575c261c949d073b2895b05d1097c3", user="MarcinKosinski", 
-#' repo="Museum", branch="master", repoDirGit="ex1")
-#' 
+#' showGithubRepo(user="pbiecek", repo="archivist")
+#' # We search for a tag with default "name" regular expression corresponding to 
+#' # "cd6557c6163a6f9800f308f343e75e72" md5hash.
+#' getTagsGithub( "cd6557c6163a6f9800f308f343e75e72",
+#'                 user="pbiecek", repo="archivist")
+#'                 
+#' ## EXAMPLE: many archivist-like Repositories on one Github repository
+#' # We search for a tag with default "name" regular expression corresponding to 
+#' # "ff575c261c949d073b2895b05d1097c3" md5hash.
+#' getTagsGithub("ff575c261c949d073b2895b05d1097c3", user="MarcinKosinski",
+#'                repo="Museum", branch="master", repoDirGit="ex1")
+#'                
 #' }
 #' 
 #' @family archivist
