501c57c4d383e49e76297025c18d5b66a29287c9
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-01-30 22:13:57 +0100

    do not duplicate

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .ArchivistEnv <- new.env()
 
-.onAttach <- function(...) {
-   packageStartupMessage("Welcome to archivist (version: ", utils::packageVersion("archivist"), ").")
+setDefaultArchivistEnv <- function() {
   .ArchivistEnv$silent <- FALSE
   .ArchivistEnv$response <- FALSE
   .ArchivistEnv$commitMessage <- NULL
@@ -15,34 +14,22 @@
   .ArchivistEnv$ascii <- FALSE
   .ArchivistEnv$branch <- "master"
   .ArchivistEnv$repoDirGit <- FALSE
-  .ArchivistEnv$alink <- FALSE
-  .ArchivistEnv$repoDescription <- "A Repository of Artifacts supported by archivist https://github.com/pbiecek"
-  .ArchivistEnv$readmeDescription <- "A Repository of Artifacts supported by [archivist](https://github.com/pbiecek) \n\n
-   [`Repository`](https://github.com/pbiecek/archivist/wiki/archivist-package-Repository) stores specific values of an artifact,
-  different for various artifact's classes and artifacts themselves. To learn more about artifacts visit [wiki](https://github.com/pbiecek/archivist/wiki)."
-}
-
-.onLoad <- function(...) {
-  assign( x = "sqlite", value = dbDriver( "SQLite" ), envir = .ArchivistEnv )
-  assign( x = ".GithubURL", value = "https://raw.githubusercontent.com", envir = .ArchivistEnv )
-  .ArchivistEnv$branch <- "master"
-  .ArchivistEnv$repoDirGit <- FALSE
-  .ArchivistEnv$silent <- FALSE
-  .ArchivistEnv$response <- FALSE
-  .ArchivistEnv$commitMessage <- NULL
-  .ArchivistEnv$archiveData <-TRUE 
-  .ArchivistEnv$archiveTags <- TRUE 
-  .ArchivistEnv$archiveMiniature <- TRUE
-  .ArchivistEnv$archiveSessionInfo <- FALSE
-  .ArchivistEnv$force <- TRUE
-  .ArchivistEnv$rememberName <- TRUE 
-  .ArchivistEnv$chain <- FALSE 
-  .ArchivistEnv$ascii <- FALSE
   .ArchivistEnv$alink <- FALSE
   .ArchivistEnv$repoDescription <- "A Repository of Artifacts supported by archivist https://github.com/pbiecek"
   .ArchivistEnv$readmeDescription <- .ArchivistEnv$readmeDescription <- "A Repository of Artifacts supported by [archivist](https://github.com/pbiecek) \n\n
-   [`Repository`](https://github.com/pbiecek/archivist/wiki/archivist-package-Repository) stores specific values of an artifact,
+  [`Repository`](https://github.com/pbiecek/archivist/wiki/archivist-package-Repository) stores specific values of an artifact,
   different for various artifact's classes and artifacts themselves. To learn more about artifacts visit [wiki](https://github.com/pbiecek/archivist/wiki)."
+}
+
+.onAttach <- function(...) {
+   setDefaultArchivistEnv()
+   packageStartupMessage("Welcome to archivist (version: ", utils::packageVersion("archivist"), ").")
+}
+
+.onLoad <- function(...) {
+  setDefaultArchivistEnv()
+  assign( x = "sqlite", value = dbDriver( "SQLite" ), envir = .ArchivistEnv )
+  assign( x = ".GithubURL", value = "https://raw.githubusercontent.com", envir = .ArchivistEnv )
 }
 
 onUnload <- function( libpath ){
