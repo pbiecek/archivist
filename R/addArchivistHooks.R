00f9d9da81cbffb83eb5606b8b1eea81da298e5b
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-02-01 23:04:00 +0100

    for #74, now more than one class is supported.

--- R/addArchivistHooks.R
+++ R/addArchivistHooks.R
@@ -6,7 +6,7 @@
 #' \code{addArchivistHook} adds a generic version of the print function for objects of selected class. 
 #' The generic function will add all object of selected class to the repo and then add hooks to the report for these objects .
 #' 
-#' @param class A character containing a name of class that should be archivised.
+#' @param class A character containing a name of class (one or more) that should be archivised.
 #' @param repo A character containing a name of a Git repository on which the Repository is archived.
 #' @param user A character containing a name of a Git user on whose account the \code{repo} is created.
 #' @param branch A character containing a name of Git Repository's branch on which the Repository is archived. 
@@ -33,28 +33,34 @@ addArchivistHook <- function(class = "ggplot",
   repoDir = NULL, 
   repo = NULL, user = NULL, branch = "master", repoDirGit = FALSE
   ){
+  stopifnot( is.character( class ), 
+             is.character( repoDir ), 
+             is.character( repo ), 
+             is.character( user ) )
   
   # set local repo to repoDir
   if (!file.exists( repoDir )) 
     createEmptyLocalRepo(repoDir)  
   setLocalRepo(repoDir)
   
-  namespace <- gsub(grep(getAnywhere(paste0("print.",class))$where, 
-                         pattern="namespace:", value=T), 
-                    pattern="namespace:", replacement="")
-  
-  if (length(namespace) == 0) {
-    stop(paste0("The function print.",class, " has not been found."))
-  }
-  
-  fun <- paste0('function(x, ...) {
+  for (class1 in class) {
+    namespace <- gsub(grep(getAnywhere(paste0("print.",class1))$where, 
+                           pattern="namespace:", value=T), 
+                      pattern="namespace:", replacement="")
+    
+    if (length(namespace) == 0) {
+      stop(paste0("The function print.",class1, " has not been found."))
+    }
+    
+    fun <- paste0('function(x, ...) {
     hash <- saveToRepo(x)
     al <- alink(hash, repo = "',repo,'", user = "',user,'", repoDirGit = ',ifelse(repoDirGit == FALSE, FALSE, paste0('"',repoDirGit,'"')),')
     cat("Load: ", al, "\n", sep="")
-    ',namespace,':::print.',class,'(x, ...)
+    ',namespace,':::print.',class1,'(x, ...)
   }')
-  
-  fun <- eval(parse(text=fun))
-  assign(paste0("print.", class), fun, pos=.GlobalEnv)
+    
+    fun <- eval(parse(text=fun))
+    assign(paste0("print.", class1), fun, pos=.GlobalEnv)
+  }
 }
 
