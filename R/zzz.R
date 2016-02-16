1cd36c3af77ea921726bdbd7387d65b00475fe37
Marcin Kosiński <m.p.kosinski@gmail.com>
2016-02-16 19:23:30 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -12,10 +12,10 @@ setDefaultArchivistEnv <- function() {
   .ArchivistEnv$commitMessage <- NULL
   .ArchivistEnv$force <- TRUE
   .ArchivistEnv$rememberName <- TRUE 
-  .ArchivistEnv$readmeDescription <- "A Repository of Artifacts supported by [archivist](https://github.com/pbiecek) \n\n
+  .ArchivistEnv$readmeDescription <- "A Repository of Artifacts supported by [archivist](https://github.com/pbiecek/archivist) \n\n
   [`Repository`](https://github.com/pbiecek/archivist/wiki/archivist-package-Repository) stores specific values of an artifact,
   different for various artifact's classes and artifacts themselves. To learn more about artifacts visit [wiki](https://github.com/pbiecek/archivist/wiki)."
-  .ArchivistEnv$repoDescription <- "A Repository of Artifacts supported by archivist https://github.com/pbiecek"
+  .ArchivistEnv$repoDescription <- "A Repository of Artifacts supported by archivist https://github.com/pbiecek/archivist"
   .ArchivistEnv$subdir <- "/"
   .ArchivistEnv$repoType <- "github"
   .ArchivistEnv$response <- FALSE
